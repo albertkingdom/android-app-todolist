46b6d99ac868f062a4ce9464a720520200c3d512
AlbertLin <albertkingdom@gmail.com>
2021-07-05 10:47:04 +0800

    basic setup

---
+++ app/src/main/java/com/example/android_app_todolist_simple/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.android_app_todolist_simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_app_todolist_simple.todolist.Todo
+import com.example.android_app_todolist_simple.todolist.TodoAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var todoAdapter: TodoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // recycler view setting
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
+
+        val todolist: MutableList<Todo> = mutableListOf()
+        todoAdapter = TodoAdapter(todolist)
+        recyclerView.adapter = todoAdapter
+
+
+        // button [addtodo] onClick
+        findViewById<Button>(R.id.btnAddtodo).setOnClickListener {
+            val todoTitle = findViewById<EditText>(R.id.newTodo).text
+
+            todoAdapter.addtodo(Todo(todoTitle.toString()))
+
+            todoTitle.clear()
+
+        }
+
+        // button [del donetodo] onCLick
+        findViewById<Button>(R.id.btnDeldonetodo).setOnClickListener {
+            todoAdapter.delDoneTodo()
+        }
+    }
+}
